308bd2c46397e6f29c61cab7151bfdacf3278107
eklem <espen.klem@gmail.com>
2026-06-19 16:25:03 +0200

    getSessions added to service worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,5 +1,25 @@
 console.log('Service Worker: Hello world without an event listener!')
 
-self.addEventListener('activate', function (eveent) {
+/* ### get Sessions, how to activate it can be figured out later. Check if new day or day past thisSession etc. */
+const getSessions = function () {
+  fetch('https://data.stortinget.no/eksport/sesjoner?format=json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log(JSON.stringify(data))
+      document.getElementById('sesjoner').innerHTML= JSON.stringify(data, false , 2)
+    })
+    .catch((error) => {
+      const span = document.createElement("span")
+      document.getElementById('sesjoner').innerHTML= `Error: ${error.message}`
+    })
+}
+
+self.addEventListener('activated', function (event) {
   console.log('Service Worker: Hello world!')
+  getSesssions()
 })
