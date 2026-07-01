403cf5a381bf46e828361a1c75e8a12f803109eb
eklem <espen.klem@gmail.com>
2026-07-01 11:06:56 +0200

    KelpUI locally https://github.com/eklem/stortinget/issues/45

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('Hello app module!')
+console.log('First try to regirster service worker.')
+
+/* ### Service worker registration */
+if ("serviceWorker" in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register("./js/service-worker.js").then(
+    (registration) => {
+      console.log("Service worker registration succeeded:", registration);
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    },
+  );
+} else {
+  console.error("Service workers are not supported.");
+}
+
+console.log('Then hello app module!')
