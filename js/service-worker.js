57a76716cb7b50785db878cdc8b906855211afcf
eklem <espen.klem@gmail.com>
2026-07-02 12:34:49 +0200

    event triggering

--- js/service-worker.js
+++ js/service-worker.js
@@ -10,6 +10,7 @@ const getSessions = function () {
       return response.json()
     })
     .then((data) => {
+      console.log('fetched data:')
       console.log(JSON.stringify(data))
     })
     .catch((error) => {
@@ -21,5 +22,19 @@ console.log('after')
 
 addEventListener('activate', function (event) {
   console.log('Service Worker: Hello world!')
+  fetch('https://data.stortinget.no/eksport/sesjoner?format=json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log('fetched data inside event listener:')
+      console.log(JSON.stringify(data))
+    })
+    .catch((error) => {
+      console.dir(error)
+    })
   getSesssions()
 })
