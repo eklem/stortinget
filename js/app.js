cff04aa4d5b2ba8d31d5fdaa07c275137b269ecd
eklem <espen.klem@gmail.com>
2026-07-07 09:31:22 +0200

    settign scope back to /stortinget/js/

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log('First try to regirster service worker:')
 if ("serviceWorker" in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
-  navigator.serviceWorker.register("/stortinget/js/service-worker.js", { scope: "/stortinget/js" }).then(
+  navigator.serviceWorker.register("/stortinget/js/service-worker.js", { scope: "/stortinget/js/" }).then(
     (registration) => {
       console.log("Service worker registration succeeded:", registration);
     },
